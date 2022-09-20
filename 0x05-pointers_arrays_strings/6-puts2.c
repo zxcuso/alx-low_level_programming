e9fae1ed1e6104893ed21c87540047530ed73f3e
zxcuso <abdulwahabyunusa4@gmail.com>
2022-09-20 05:03:38 -0700

    66

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; ++i)
+	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
-			_putchar(str[i]);
+			putchar(*(str + i));
+		i++;
 	}
-	_putchar('\n');
+	putchar(10);
 }
