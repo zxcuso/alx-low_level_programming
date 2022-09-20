c72c9a765e5f962533c45da545af4cdf41993f7e
zxcuso <abdulwahabyunusa4@gmail.com>
2022-09-20 02:24:00 -0700

    hdg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * 0-reset_to_98.c - updates the value it points to to 98
- * @n: parameter
+ * 0-reset_to_98.c - takes a pointer to an int parameter and updates the value it points to 98
+ * @n: int parameter
  * Return : nothing
  */
 
