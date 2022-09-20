c16bc992e5f33d239a91f9aacadc43530205ab16
zxcuso <abdulwahabyunusa4@gmail.com>
2022-09-20 02:58:31 -0700

    tf

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers  
+ * swap_int-swaps the values of two integers  
  * @a: first intetger
  * @b: second integer
  * Return: Nothing
