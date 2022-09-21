5516b8944dda0c96aa063ac7437470810bd6502c
zxcuso <abdulwahabyunusa4@gmail.com>
2022-09-21 15:19:11 -0700

    fg

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 /**
  * void prototypes()
  * int prototypes()
+ * char prototypes()
  */
 
 char *_strcat(char *dest, char *src);
@@ -17,6 +19,7 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add strings(char *n1, char *n2, char *r, int r_index);
 void print_buffer(char *b, int size);
 
 #endif /*MAIN_H*/
