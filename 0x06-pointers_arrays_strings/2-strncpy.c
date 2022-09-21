fb49e7e95634486f9dd3b35ea8ecb46025a04533
zxcuso <abdulwahabyunusa4@gmail.com>
2022-09-21 16:45:21 -0700

    1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - c function
+ * terminating null byte
+ * if the lenght of the source 
+ * the reminder of the 
+ * works identically to the standard
+ * @dest: buffer
+ * @src: the  source
+ * @n: max number
+ * Return: rturn
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
